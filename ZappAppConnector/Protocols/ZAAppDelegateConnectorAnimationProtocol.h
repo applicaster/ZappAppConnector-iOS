8e7cd769b54cc902c4a51c57315fe68709686695
Alex Zchut <a.zchut@applicaster.com>
2017-09-03 15:31:27 +0300

    update descriptions

--- ZappAppConnector/Protocols/ZAAppDelegateConnectorAnimationProtocol.h
+++ ZappAppConnector/Protocols/ZAAppDelegateConnectorAnimationProtocol.h
@@ -2,7 +2,7 @@
 //  ZAAppDelegateConnectorAnimationProtocol.h
 //  ZappAppConnector
 //
-//  Created by user on 03/07/2017.
+//  Created by Alex Zchut on 11/07/2017.
 //  Copyright © 2017 Applicaster Ltd. All rights reserved.
 //
 
--- ZappAppConnector/Protocols/ZAAppDelegateConnectorURLProtocol.h
+++ ZappAppConnector/Protocols/ZAAppDelegateConnectorURLProtocol.h
@@ -2,9 +2,10 @@
 //  ZAAppDelegateConnectorURLProtocol.h
 //  ZappAppConnector
 //
-//  Created by user on 03/07/2017.
+//  Created by Alex Zchut on 11/07/2017.
 //  Copyright © 2017 Applicaster Ltd. All rights reserved.
 //
+
 
 @protocol ZAAppDelegateConnectorURLProtocol
 
